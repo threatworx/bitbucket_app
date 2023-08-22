6b0a8cd65772e10480fbbcad38dca5b3128a9745
Ketan Nilangekar <ketonne@yahoo.com>
2023-08-22 18:42:15 +0000

    Test commit for workflow

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 # Install bitbucket app dependencies
 pip install -r /tmp/requirements.txt
+
 # Setup twigs update script
 printf "#!/bin/bash\n/usr/share/bitbucket_app/gen_def_cert.sh\n/usr/local/bin/uwsgi --ini /opt/tw_bitbucket_app/config/uwsgi.ini" > /usr/local/bin/run-app.sh
 chmod +x /usr/local/bin/run-app.sh
